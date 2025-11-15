b7414a666481821aee435f16411cf8bc58ac58cd
Manukrishnan G <g.manukrishnan@gmail.com>
2025-11-15 21:44:10 +0400

    new changes

--- test_greeter.py
+++ test_greeter.py
@@ -4,7 +4,7 @@ from greeter import greet
 class TestGreet(unittest.TestCase):
 
     def test_greet_with_name(self):
-        self.assertEqual(greet("Alice"), "Welcome, Alice!!")
+        self.assertEqual(greet("Alice"), "Welcome, Alice!")
 
     def test_greet_without_name(self):
         self.assertEqual(greet(), "Welcome!")
